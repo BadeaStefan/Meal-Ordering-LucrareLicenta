da1431907909b6e79301de1809432db616148198
BadeaStefan <79524984+BadeaStefan@users.noreply.github.com>
2024-06-25 20:30:33 +0300

    Commit4

--- src/pages/MealInfoPage.jsx
+++ src/pages/MealInfoPage.jsx
@@ -1,16 +1,8 @@
 import MealForm from '../components/MealForm.jsx';
 
-<<<<<<< HEAD
 export default function MealInfoPage() {
 
 	return (
 		<MealForm method="post" ></MealForm>
-=======
-
-
-export default function MealInfoPage(){
-	return(
-		<MealForm method="post"></MealForm>
->>>>>>> c8913fe45af73e1ac519f7c78baebea92b5cd503
-	);
-}
+	)
+		}
